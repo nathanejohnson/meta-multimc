c50c07859aec39cc77dcba56b04d6dfc8e0449f7
Nathan Johnson <nathan@nathanjohnson.org>
2021-04-17 20:27:56 -0500

    update copier

--- copy_meta_multimc.sh
+++ copy_meta_multimc.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 cd "$(dirname "$0")"
+# clone https://github.com/MultiMC/meta-multimc.git
 UPSTREAM_REPO_PATH="${1:-../upstream-meta-multimc}"
-if [ ! -d "${UPSTREAM_REPO_PATH}" ]; then
-	echo "${UPSTREAM_REPO_PATH}" does not exist
-	exit 1
+if [ -d "${UPSTREAM_REPO_PATH}" ]; then
+	cd "${UPSTREAM_REPO_PATH}"
+	git pull
+	cd -
+else
+	git clone https://github.com/MultiMC/meta-multimc.git "${UPSTREAM_REPO_PATH}"
 fi
 
 DIRS=$(cd "${UPSTREAM_REPO_PATH}";find . -type d ! -path './.git/*' ! -path './.git' ! -path './org.lwjgl3' ! -path './org.lwjgl' ! -path '.')
